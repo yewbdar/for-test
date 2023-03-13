ecad4e84d8fb716802c3a99810769fa376e2912e
yewbdar <32113737+yewbdar@users.noreply.github.com>
2023-03-13 15:14:21 -0500

    Update for test.jsx

--- insight /for test.jsx
+++ insight /for test.jsx
@@ -6,3 +6,26 @@ const showModalChange = (isShow) => {
       selectedLowBal: ''
     })
   }
+
+
+
+
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { showModalChange } from './your-file-name'
+
+describe('showModalChange function', () => {
+  it('should update the showModal state and setSelectedInsightData with the correct values', () => {
+    const setShowModal = jest.fn()
+    const setSelectedInsightData = jest.fn()
+
+    showModalChange(true, setShowModal, setSelectedInsightData)
+
+    expect(setShowModal).toHaveBeenCalledWith(true)
+    expect(setSelectedInsightData).toHaveBeenCalledWith({
+      showModal: true,
+      selectedInsight: '',
+      selectedLowBal: ''
+    })
+  })
+})
