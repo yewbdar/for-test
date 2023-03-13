c3a6dba74258c65563718e58a968676c539bed91
Yewbdar Abera <Yewbdar.Abera@usbank.com>
2023-03-13 09:16:02 -0500

    test

---
+++ insight /LowBalanceModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ErrorBoundary from '../ErrorBoundary'
+
+const LowBalanceModal = (props) => {
+  const LowBalAlertWidget = React.lazy(() => import('Insights/LowBalance'))
+  return (
+    <ErrorBoundary>
+      <LowBalAlertWidget {...props} />
+    </ErrorBoundary>
+  )
+}
+
+export { LowBalanceModal }
