91f8a487679bbbe1c2e7676b0df9768cd38ca4f5
yewbdar <32113737+yewbdar@users.noreply.github.com>
2023-03-13 10:03:01 -0500

    Update LowBalanceModal.jsx

--- insight /LowBalanceModal.jsx
+++ insight /LowBalanceModal.jsx
@@ -11,3 +11,31 @@
 // }
 
 // export { LowBalanceModal }
+
+
+
+
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { LowBalanceModal } from './LowBalanceModal';
+
+jest.mock('Insights/LowBalance', () => {
+  return {
+    __esModule: true,
+    default: ({ amount }) => (
+      <div data-testid="low-balance-widget">
+        Your balance is low: ${amount}
+      </div>
+    ),
+  };
+});
+
+describe('LowBalanceModal', () => {
+  test('renders the low balance widget with the given amount', async () => {
+    render(<LowBalanceModal amount={100} />);
+
+    const widget = await screen.findByTestId('low-balance-widget');
+    expect(widget).toHaveTextContent('Your balance is low: $100');
+  });
+});
