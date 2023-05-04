8998fef955e76e4863991798c546d40b0f268cf8
yewbdar <32113737+yewbdar@users.noreply.github.com>
2023-05-04 13:27:28 -0500

    removed

--- insight /LowBalanceAlertSuccessCard.jsx
+++ insight /LowBalanceAlertSuccessCard.jsx
@@ -1,27 +1,2 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import CustomTooltip from './CustomTooltip';
 
-describe('CustomTooltip', () => {
-  it('renders tooltip correctly', () => {
-    const props = {
-      payload: [
-        {
-          payload: {
-            month: 'January',
-            value: 1000
-          }
-        }
-      ]
-    };
-
-    render(<CustomTooltip {...props} />);
-
-    const dateLabel = screen.getByText(/January/i);
-    expect(dateLabel).toBeInTheDocument();
-
-    const accountBalance = screen.getByText(/\$1,000/i);
-    expect(accountBalance).toBeInTheDocument();
-  });
-});
 
